47b9c3b6067a32d72b7bd38ad9f35b0fd4468d2e
Daniel Hanusey <tud51324@gmail.com>
2018-09-20 11:07:47 -0400

    Modified Cancel() to return Map object. Created initMap()

--- src/paystation/domain/PayStation.java
+++ src/paystation/domain/PayStation.java
@@ -19,6 +19,8 @@
  */
 package paystation.domain;
 
+import java.util.Map;
+
 public interface PayStation {
 
     /**
@@ -48,6 +50,12 @@ public interface PayStation {
 
     /**
      * Cancel the present transaction. Resets the machine for a new transaction.
+     * @return a map of deposited coins.
      */
-    public void cancel();
+    public Map<Integer,Integer> cancel();
+    
+     /**
+     * Initiate map used for tracking coins deposited
+     */
+    public void initMap();
 }
